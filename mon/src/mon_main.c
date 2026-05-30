101fecd8663bf6e637449dd4dd9e150b442425b9
alwaez01 <yaroslavvakyla@gmail.com>
2026-05-30 13:58:21 +0200

    use CLOCK_REALTIME for time calculations

--- mon/src/mon_main.c
+++ mon/src/mon_main.c
@@ -12,7 +12,7 @@ static long long get_time_ms(void)
 {
      struct timespec ts;
 
-     clock_gettime(CLOCK_MONOTONIC, &ts);
+     clock_gettime(CLOCK_REALTIME, &ts);
      return ts.tv_sec * 1000LL + ts.tv_nsec / 1000000LL;
 }
 
